9ff38bd14587bd6fc34c1e31d345b5b497d452a0
anoju <zun5761@gmail.com>
2023-02-17 09:27:38 +0900

    first

---
+++ pages/home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>home page</h2>
+    <ul>
+      <li>
+        <NuxtLink to="/" class="button line">move index</NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <uiPage page-title="페이지타이틀" btn-back>
+    <uiInner>
+      <h1>Index page</h1>
+      <ul>
+        <li>
+          <NuxtLink to="/home" class="button line">move home</NuxtLink>
+        </li>
+      </ul>
+    </uiInner>
+  </uiPage>
+</template>
