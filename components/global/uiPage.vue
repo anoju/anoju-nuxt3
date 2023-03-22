f12245a1e0b5f30999a602c20970ba0ef34e4b69
anoju <zun5761@gmail.com>
2023-03-22 14:09:42 +0900

    컴포넌트 추가

--- components/global/uiPage.vue
+++ components/global/uiPage.vue
@@ -6,13 +6,13 @@
       </div>
       <div v-else>
         <div v-if="!!btnBack || !!$slots.headLeft" class="head-left">
-          <button v-if="!!btnBack" type="button" class="head-back button" aria-label="이전화면"></button>
+          <button v-if="!!btnBack" type="button" class="head-back button" aria-label="이전화면" @click="backClick"></button>
           <slot name="headLeft" />
         </div>
         <h1 v-if="pageTitle">{{ pageTitle }}</h1>
         <div v-if="!!btnClose || !!$slots.headRight" class="head-right">
           <slot name="headRight" />
-          <button v-if="!!btnClose" type="button" class="head-close button" role="button" aria-label="화면닫기"></button>
+          <button v-if="!!btnClose" type="button" class="head-close button" role="button" aria-label="화면닫기" @click="closeClick"></button>
         </div>
       </div>
     </header>
@@ -42,8 +42,30 @@ export default {
     headClass: { type: [String, Object], default: null },
     bodyClass: { type: [String, Object], default: null },
     noHeader: { type: Boolean, default: false },
-    btnBack: { type: Boolean, default: false },
-    btnClose: { type: Boolean, default: false }
+    btnBack: { type: [Boolean, String, Function], default: false },
+    btnClose: { type: [Boolean, String, Function], default: false }
+  },
+  methods: {
+    backClick() {
+      const router = useRouter();
+      if (typeof this.btnBack === 'function') {
+        this.btnBack();
+      } else if (typeof this.btnBack === 'string') {
+        router.push({ path: this.btnBack });
+      } else {
+        router.back();
+      }
+    }
+  },
+  closeClick() {
+    const router = useRouter();
+    if (typeof this.btnClose === 'function') {
+      this.btnClose();
+    } else if (typeof this.btnClose === 'string') {
+      router.push({ path: this.btnClose });
+    } else {
+      router.back();
+    }
   }
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,8 @@
 <template>
   <uiPage page-title="Index page" btn-back>
     <uiInner>
-      <ul>
-        <li>
-          <NuxtLink to="/home" class="button line">move home</NuxtLink>
-        </li>
-      </ul>
+      <p><NuxtLink to="/home" class="button line">move home</NuxtLink></p>
+      <p><uiButton line>버튼</uiButton></p>
     </uiInner>
   </uiPage>
 </template>
