6ec54d202c4d55322ad9d2d557e61ad0919ee5da
anoju <zun5761@gmail.com>
2023-04-03 16:05:49 +0900

    eventBus

---
+++ utils/eventBus.ts
@@ -0,0 +1,58 @@
+import { inject, provide, reactive } from 'vue';
+
+interface EventBus {
+  events: Record<string, Function[]>;
+  $on(event: string, callback: Function): void;
+  $off(event: string, callback: Function): void;
+  $emit(event: string, ...args: any[]): void;
+}
+
+const eventBusSymbol = Symbol('eventBus');
+
+export function createEventBus(): EventBus {
+  const events: Record<string, any> = reactive({});
+
+  function $on(event: string, callback: Function) {
+    if (!events[event]) {
+      events[event] = [];
+    }
+    events[event].push(callback);
+  }
+
+  function $off(event: string, callback: Function) {
+    if (events[event]) {
+      const index = events[event].indexOf(callback);
+      if (index !== -1) {
+        events[event].splice(index, 1);
+      }
+    }
+  }
+
+  function $emit(event: string, ...args: any[]) {
+    if (events[event]) {
+      events[event].forEach((callback: Function) => {
+        callback(...args);
+      });
+    }
+  }
+
+  return {
+    events,
+    $on,
+    $off,
+    $emit
+  };
+}
+
+export function provideEventBus() {
+  const eventBus = createEventBus();
+  provide(eventBusSymbol, eventBus);
+}
+
+export function useEventBus(): EventBus {
+  const eventBus = inject<EventBus>(eventBusSymbol);
+  if (!eventBus) {
+    throw new Error('Event bus not provided!');
+  }
+  return eventBus;
+}
