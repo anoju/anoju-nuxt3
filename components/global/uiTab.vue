e59b4cb77a928919744ad429520a975b4e337246
anoju <zun5761@gmail.com>
2023-04-07 11:05:41 +0900

    uiTab

--- components/global/uiTab.vue
+++ components/global/uiTab.vue
@@ -6,17 +6,21 @@ const props = defineProps({
 const activeTab = inject<Ref<number | string> | undefined>('activeTab');
 const setActiveTab = inject<(index: number | string) => void | undefined>('setActiveTab');
 const modelValueType = inject<string | undefined>('modelValueType');
+
 if (!activeTab || !setActiveTab || !modelValueType) {
   throw new Error('uiTab component must be a child of uiTabs component');
 }
 
-const isActive = ref(false);
-const isEqual = (a: number | string, b: number | string) => {
+const isEqual = (a: number | string, b: number | string): boolean => {
   return modelValueType === 'number' ? Number(a) === Number(b) : a === b;
 };
+const firstActive = isEqual(props.index, activeTab.value);
+const isActive = ref(firstActive);
+
 onMounted(() => {
   isActive.value = isEqual(props.index, activeTab.value);
 });
+
 watchEffect(() => {
   isActive.value = isEqual(props.index, activeTab.value);
 });
@@ -24,6 +28,7 @@ watchEffect(() => {
 const selectTab = () => {
   setActiveTab(props.index);
 };
+
 defineExpose({ isActive, selectTab });
 </script>
 <template>
