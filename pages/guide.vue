1cdc7c67e7b0a39e21b73011b9e744e917cf005f
anoju <zun5761@gmail.com>
2023-04-20 17:51:53 +0900

    layout 수정

--- pages/guide.vue
+++ pages/guide.vue
@@ -6,17 +6,28 @@ definePageMeta({
   //   exclude: ['guide/font']
   // }
 });
+const isTabShow = ref(true);
+const route = useRoute();
+watch(
+  route,
+  (value) => {
+    if (value.path.indexOf('/font') === -1) {
+      isTabShow.value = true;
+    } else {
+      isTabShow.value = false;
+    }
+  },
+  { deep: true, immediate: true }
+);
 </script>
 <template>
   <uiPage page-title="Guide" btn-back>
-    <uiTabs>
+    <uiTabs v-if="isTabShow">
       <uiTab to="/guide/text">text</uiTab>
       <uiTab to="/guide/button">button</uiTab>
       <uiTab to="/guide/form">form</uiTab>
       <uiTab to="/guide/popup">popup</uiTab>
     </uiTabs>
-    <uiInner>
-      <NuxtPage />
-    </uiInner>
+    <NuxtPage />
   </uiPage>
 </template>
