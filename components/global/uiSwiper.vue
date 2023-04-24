38de84b14098326f3cd977554f1fff5fcbd19286
anoju <zun5761@gmail.com>
2023-04-24 09:21:02 +0900

    swiper

---
+++ components/global/uiSwiper.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import Swiper, { Swiper as SwiperInstance } from 'swiper';
+const props = defineProps({
+  pagination: { type: Boolean, default: true },
+  navi: { type: Boolean, default: true }
+});
+
+const swiperInstance = ref<SwiperInstance | null>(null);
+
+onMounted(() => {
+  if (swiperInstance.value) {
+    swiperInstance.value = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      }
+    });
+  }
+});
+</script>
+<template>
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <slot />
+    </div>
+    <div v-if="pagination" class="swiper-pagination"></div>
+    <div v-if="navi" class="swiper-button-next"></div>
+    <div v-if="navi" class="swiper-button-prev"></div>
+  </div>
+</template>
