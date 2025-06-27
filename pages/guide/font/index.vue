a06d7a929e2f02e4d1961ac306a67f82197f5e42
zun5761 <zun5761@gmail.com>
2025-06-27 09:21:07 +0900

    폰트 최신화 작업중

--- pages/guide/font/index.vue
+++ pages/guide/font/index.vue
@@ -5,75 +5,32 @@ useHead({
 });
 const emit = defineEmits(['page-title']);
 emit('page-title', pageTitle);
+
+const fontWeightType = (i) => {
+  if (i === 1) return 'Thin';
+  if (i === 2) return 'ExtraLight ';
+  if (i === 3) return 'Light';
+  if (i === 4) return 'Regular';
+  if (i === 5) return 'Medium';
+  if (i === 6) return 'SemiBold';
+  if (i === 7) return 'Bold';
+  if (i === 8) return 'ExtraBold';
+  if (i === 9) return 'Black';
+};
 </script>
 <template>
   <uiInner>
     <h1 class="gd__h1">font</h1>
 
-    <h3 class="gd__h3">Noto Sans KR - v27 (숫자+콤마 외 모두 적용)</h3>
+    <h3 class="gd__h3">Noto Sans KR - v37 (숫자+콤마 외 모두 적용)</h3>
     <div class="fz-20">
       <div class="gd__tbl">
         <table>
           <tbody>
-            <tr>
-              <th>Thin</th>
+            <tr v-for="i in 9" :key="i">
+              <th>{{ fontWeightType(i) }}</th>
               <td>
-                <div class="fw-100">
-                  <div>가나다라마바사아자차카타파하</div>
-                  <div>ABCDEFGHIJKLMNOPQRSTUVWXYZ</div>
-                  <div>abcdefghijklmnopqrstuvwxyz</div>
-                  <div>!@#$%^&*?'";:[]{}&lt;&gt;()-=_+/~`.\|</div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th>Light</th>
-              <td>
-                <div class="fw-300">
-                  <div>가나다라마바사아자차카타파하</div>
-                  <div>ABCDEFGHIJKLMNOPQRSTUVWXYZ</div>
-                  <div>abcdefghijklmnopqrstuvwxyz</div>
-                  <div>!@#$%^&*?'";:[]{}&lt;&gt;()-=_+/~`.\|</div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th>Regular</th>
-              <td>
-                <div class="fw-400">
-                  <div>가나다라마바사아자차카타파하</div>
-                  <div>ABCDEFGHIJKLMNOPQRSTUVWXYZ</div>
-                  <div>abcdefghijklmnopqrstuvwxyz</div>
-                  <div>!@#$%^&*?'";:[]{}&lt;&gt;()-=_+/~`.\|</div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th>Medium</th>
-              <td>
-                <div class="fw-500">
-                  <div>가나다라마바사아자차카타파하</div>
-                  <div>ABCDEFGHIJKLMNOPQRSTUVWXYZ</div>
-                  <div>abcdefghijklmnopqrstuvwxyz</div>
-                  <div>!@#$%^&*?'";:[]{}&lt;&gt;()-=_+/~`.\|</div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th>Bold</th>
-              <td>
-                <div class="fw-700">
-                  <div>가나다라마바사아자차카타파하</div>
-                  <div>ABCDEFGHIJKLMNOPQRSTUVWXYZ</div>
-                  <div>abcdefghijklmnopqrstuvwxyz</div>
-                  <div>!@#$%^&*?'";:[]{}&lt;&gt;()-=_+/~`.\|</div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th>Black</th>
-              <td>
-                <div class="fw-900">
+                <div :class="'fw-' + i * 100">
                   <div>가나다라마바사아자차카타파하</div>
                   <div>ABCDEFGHIJKLMNOPQRSTUVWXYZ</div>
                   <div>abcdefghijklmnopqrstuvwxyz</div>
@@ -86,60 +43,15 @@ emit('page-title', pageTitle);
       </div>
     </div>
 
-    <h3 class="gd__h3">Roboto - v30 (숫자+콤마만 적용)</h3>
+    <h3 class="gd__h3">Roboto - v48 (숫자+콤마만 적용)</h3>
     <div class="fz-20">
       <div class="gd__tbl">
         <table>
           <tbody>
-            <tr>
-              <th>Thin</th>
+            <tr v-for="i in 9" :key="i">
+              <th>{{ fontWeightType(i) }}</th>
               <td>
-                <div class="fw-100">
-                  <div>1234567890,</div>
-                  <div class="ff-noto">1234567890, <sub class="fc-888 fz-16">(Noto Sans KR:비교용)</sub></div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th>Light</th>
-              <td>
-                <div class="fw-300">
-                  <div>1234567890,</div>
-                  <div class="ff-noto">1234567890, <sub class="fc-888 fz-16">(Noto Sans KR:비교용)</sub></div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th>Regular</th>
-              <td>
-                <div class="fw-400">
-                  <div>1234567890,</div>
-                  <div class="ff-noto">1234567890, <sub class="fc-888 fz-16">(Noto Sans KR:비교용)</sub></div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th>Medium</th>
-              <td>
-                <div class="fw-500">
-                  <div>1234567890,</div>
-                  <div class="ff-noto">1234567890, <sub class="fc-888 fz-16">(Noto Sans KR:비교용)</sub></div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th>Bold</th>
-              <td>
-                <div class="fw-700">
-                  <div>1234567890,</div>
-                  <div class="ff-noto">1234567890, <sub class="fc-888 fz-16">(Noto Sans KR:비교용)</sub></div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th>Black</th>
-              <td>
-                <div class="fw-900">
+                <div :class="'fw-' + i * 100">
                   <div>1234567890,</div>
                   <div class="ff-noto">1234567890, <sub class="fc-888 fz-16">(Noto Sans KR:비교용)</sub></div>
                 </div>
