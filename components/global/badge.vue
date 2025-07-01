23ccaa2f394de1f3224dccb5e429d0e062904590
zun5761 <zun5761@gmail.com>
2025-07-01 17:25:05 +0900

    badge 컴포넌트 업데이트

--- components/global/badge.vue
+++ components/global/badge.vue
@@ -1,71 +1,106 @@
 <script lang="ts" setup>
-const props = defineProps({
-  ty2: { type: Boolean, default: false },
-  ty3: { type: Boolean, default: false },
-  line: { type: Boolean, default: false },
-  line2: { type: Boolean, default: false },
-  square: { type: Boolean, default: false },
-  round: { type: Boolean, default: false },
+// Types
+interface Props {
+  ty2?: boolean;
+  ty3?: boolean;
+  line?: boolean;
+  line2?: boolean;
+  square?: boolean;
+  round?: boolean;
 
   // 색상
-  color: { type: String, default: null },
-  primary: { type: Boolean, default: false },
-  gray: { type: Boolean, default: false },
-  gray2: { type: Boolean, default: false },
-  red: { type: Boolean, default: false },
-  green: { type: Boolean, default: false },
-  blue: { type: Boolean, default: false },
-  blue2: { type: Boolean, default: false },
-  orange: { type: Boolean, default: false },
-  black: { type: Boolean, default: false },
+  color?: string | null;
+  primary?: boolean;
+  gray?: boolean;
+  gray2?: boolean;
+  red?: boolean;
+  green?: boolean;
+  blue?: boolean;
+  blue2?: boolean;
+  orange?: boolean;
+  black?: boolean;
 
-  //size
-  size: { type: String, default: null },
-  small: { type: Boolean, default: false },
-  large: { type: Boolean, default: false },
-  h28: { type: Boolean, default: false }
-});
+  // 크기
+  size?: string | null;
+  small?: boolean;
+  large?: boolean;
+  h28?: boolean;
+}
 
 type Color = 'primary' | 'gray' | 'gray2' | 'red' | 'green' | 'blue' | 'blue2' | 'orange' | 'black';
+type Size = 'small' | 'large' | 'h28';
+
+// Props with defaults using withDefaults
+const props = withDefaults(defineProps<Props>(), {
+  ty2: false,
+  ty3: false,
+  line: false,
+  line2: false,
+  square: false,
+  round: false,
+  color: null,
+  primary: false,
+  gray: false,
+  gray2: false,
+  red: false,
+  green: false,
+  blue: false,
+  blue2: false,
+  orange: false,
+  black: false,
+  size: null,
+  small: false,
+  large: false,
+  h28: false
+});
+
+// Computed properties
 const colorAry: Color[] = ['primary', 'gray', 'gray2', 'red', 'green', 'blue', 'blue2', 'orange', 'black'];
-const matchingColor = colorAry.find((color) => props[color]);
-const $color = computed<Color | null>((): Color | null => {
+const matchingColor = computed(() => colorAry.find((color) => props[color]));
+const $color = computed<Color | null>(() => {
   if (props.color && colorAry.includes(props.color as Color)) {
     return props.color as Color;
-  } else if (matchingColor) {
-    return matchingColor;
   }
-  return null;
+  return matchingColor.value || null;
 });
 
-type Size = 'small' | 'large' | 'h28';
 const sizeAry: Size[] = ['small', 'large', 'h28'];
-const matchingSize = sizeAry.find((size) => props[size]);
-const $size = computed<Size | null>((): Size | null => {
+const matchingSize = computed(() => sizeAry.find((size) => props[size]));
+const $size = computed<Size | null>(() => {
   if (props.size && sizeAry.includes(props.size as Size)) {
     return props.size as Size;
-  } else if (matchingSize) {
-    return matchingSize;
   }
-  return null;
+  return matchingSize.value || null;
 });
 
-const badgeClass = computed<Array<string | Object>>((): Array<string | Object> => {
-  const rtnAry = ['badge', { ty2: props.ty2, ty3: props.ty3 }];
+const badgeClass = computed<Array<string | Record<string, boolean>>>(() => {
+  const rtnAry: Array<string | Record<string, boolean>> = [
+    'badge', 
+    { 
+      ty2: props.ty2, 
+      ty3: props.ty3 
+    }
+  ];
+  
   if (!props.ty2 && !props.ty3) {
-    const $baseOpt: any = {
+    const baseOptions: Record<string, boolean> = {
       line: props.line,
       line2: props.line2,
       square: props.square,
       round: props.round
     };
-    rtnAry.push($baseOpt);
+    rtnAry.push(baseOptions);
   }
+  
   if ($color.value) rtnAry.push($color.value);
   if ($size.value) rtnAry.push($size.value);
+  
   return rtnAry;
 });
 </script>
+
 <template>
-  <i :class="badgeClass"><slot /></i>
+  <i :class="badgeClass">
+    <slot />
+  </i>
 </template>
